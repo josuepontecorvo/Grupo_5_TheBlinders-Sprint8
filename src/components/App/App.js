5905eec774c02b7e8b40fdcec9857b00c82c3897
Josue Pontecorvo <josuepontecorvo@gmail.com>
2022-10-20 13:16:05 -0300

    login guard added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { ContentWrapper } from '../ContentWrapper';
 import Dashboard from '../../pages/Dashboard';
 import { SideBar } from '../SideBar';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from '../../pages/Login/Login';
+import AuthGuard from '../../guards/log.guard';
 
 function App() {
 
@@ -10,7 +13,14 @@ function App() {
       <div id='wrapper'>
         <SideBar />
         <ContentWrapper>
-          <Dashboard />
+          <Routes>
+            <Route path="/" element={ <Navigate to="/dashboard" /> } />
+            <Route path="*" element={ <> Not Found </> } />
+            <Route path="/login" element={ <Login /> } />
+            <Route element={ <AuthGuard />}>
+              <Route path="/dashboard" element={ <Dashboard /> } />
+            </Route>
+          </Routes>
         </ContentWrapper>
       </div>
     </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import {App} from './components/App';
 import './assets/css/app.css';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
