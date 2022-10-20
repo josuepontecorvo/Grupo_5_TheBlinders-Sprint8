cffa2f3197982b01a9a25c0479fe0fdf003ea728
Josue Pontecorvo <josuepontecorvo@gmail.com>
2022-10-20 12:48:02 -0300

    Login page added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,34 +1,10 @@
-import React from 'react';
-import { useEffect, useState } from "react";
-import ContentRowDb from '../../pages/ContentRowDb'
-import LastProductInDb from '../../pages/LastProductInDb';
+import React from "react";
+import { Login } from "../../pages/Login";
+
 function App() {
-  const [detail, setDetail] = useState('/api/productos/1')
-  const [lastProduct, setLastProduct] = useState({})
-
-  useEffect(() => {
-    let url = 'http://localhost:3000/api/productos'
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        setDetail(data.data[data.data.length - 1].detail)
-      })
-    let detailUrl = `http://localhost:3000${detail}`
-    fetch(detailUrl)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.data)
-        setLastProduct(data.data)
-      })
-  }, [detail])
-
-  /*let i = products.length - 1;
-  let lastProduct = products[i]*/
-
-  return (
+    return (
     <React.Fragment>
-      <ContentRowDb />
-      <LastProductInDb  {...lastProduct}/>
+      <Login></Login>
     </React.Fragment>
   );
 }
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,56 @@
+import { useRef } from "react"
+import { useDispatch } from "react-redux";
+
+function Login() {
+    // const dispatch = useDispatch();
+    let inputEmail = useRef()
+    let inputPassword = useRef()
+    let feedback = useRef()
+
+    function handleSubmit() {
+
+        let username = inputEmail.current.value
+        let password = inputPassword.current.value
+        let credentials = {username, password}
+        console.log(credentials)
+        fetch("http://localhost:3000/api/usuarios/ingresar", {method: 'POST', headers: {'Content-Type': 'application/json'},body: JSON.stringify(credentials)})
+            .then(res => res.json())
+            .then(data => {
+                if (data.error) {
+                    feedback.current.innerText = 'Credenciales invalidas'
+                } else (
+                    feedback.current.innerText = ''
+                    // dispatch(createUser({...data.data}))
+                )
+            })
+
+
+        
+
+    }
+
+    return (
+    <div className="container vh-100 d-flex align-items-center">
+        <div className="col-8 mx-auto position-relative">
+            <h2 className="text-center my-5">Login</h2>
+            <div className="mb-3 row">
+                <label for="staticEmail" className="col-lg-2 col-form-label">Email</label>
+                <div className="col-lg-10">
+                    <input ref={inputEmail} type="text" className="form-control" id="inputEmail" placeholder="email@example.com" />
+                </div>
+            </div>
+            <div className="mb-3 row">
+                <label for="inputPassword" className="col-lg-2 col-form-label">Password</label>
+                <div className="col-lg-10">
+                    <input ref={inputPassword} type="password" className="form-control" id="inputPassword" />
+                </div>
+            </div>
+            <small ref={feedback} className="w-100 text-center position-absolute text-danger"></small>
+            <div className="d-flex mt-5">
+                <button type="submit" onClick={handleSubmit} className="btn btn-primary col-lg-12">Confirm</button>
+            </div>
+        </div>
+    </div>
+    )
+}
+export default Login
